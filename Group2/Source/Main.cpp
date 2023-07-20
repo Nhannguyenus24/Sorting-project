bef6ea46a5f701b12ae606f3de9798cf413a28e6
Calvadoss <124347873+Nhannguyenus24@users.noreply.github.com>
2023-07-20 13:39:58 +0700

    Update Main.cpp

--- Group2/Source/Main.cpp
+++ Group2/Source/Main.cpp
@@ -1,8 +1,6 @@
 #include "Function.h"
-#include "Sort.cpp"
-#include "sort_with_count_compare.cpp"
-#include "Function.cpp"
-int main(int argc, char* argv[]){
-    command(argc, argv);
-    return 0;
+
+int main(int argc, char* argv[]) {
+	command(argc, argv);
+	return 0;
 }
